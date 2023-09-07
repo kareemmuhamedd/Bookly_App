f0bb3b537d2c7a65c44e673925f68b83f1c9e254
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-09-07 22:28:45 +0300

    create splash screen

---
+++ lib/features/splach/presentation/views/widgets/splash_veiw_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenBody extends StatelessWidget {
+  const SplashScreenBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
