b9aea318394f9f9773a0828a8500721c53f98d61
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-09-09 00:08:10 +0300

    add featured list view to home

--- lib/features/home/presetation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presetation/views/widgets/custom_list_view_item.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assets.dart';
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({Key? key}) : super(key: key);
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.height * 0.3,
-      child: AspectRatio(
-        aspectRatio: 2.8 / 4,
-        child: Container(
-          decoration:  BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.green,
-            image: const DecorationImage(
-                image: AssetImage(
-                  AssetsData.testImage,
-                ),
-                fit: BoxFit.fill),
-          ),
+    return AspectRatio(
+      aspectRatio: 2.8 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.green,
+          image: const DecorationImage(
+              image: AssetImage(
+                AssetsData.testImage,
+              ),
+              fit: BoxFit.fill),
         ),
       ),
     );
--- lib/features/home/presetation/views/widgets/home_screen_body.dart
+++ lib/features/home/presetation/views/widgets/home_screen_body.dart
@@ -11,9 +11,29 @@ class HomeScreenBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(),
-          CustomListViewItem(),
+          FeaturedBooksListView(),
         ],
       ),
+    );
+  }
+}
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height * 0.3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 15),
+              child: FeaturedListViewItem(),
+            );
+          }),
     );
   }
 }
