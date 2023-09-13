3dc83e2baf54ddff8a615ae771f273f0bfbfad73
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-09-14 00:44:04 +0300

    create app font styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const kFontSectraFine = 'GT Sectra Fine';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
+  static const titleMedium18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+  static const titleMedium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const titleNormal20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kFontSectraFine,
+  );
+  static const titleNormal30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: kFontSectraFine,
+  );
+  static const titleNormal14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
   );
 }
--- lib/features/home/presetation/views/widgets/home_screen_body.dart
+++ lib/features/home/presetation/views/widgets/home_screen_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/assets.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -19,10 +20,40 @@ class HomeScreenBody extends StatelessWidget {
             SizedBox(height: 20),
             Text(
               'Best seller',
-              style: Styles.titleMedium,
-            )
+              style: Styles.titleMedium18,
+            ),
+            BestSellerListViewItem()
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 126,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.green,
+                image: const DecorationImage(
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    ),
+                    fit: BoxFit.fill),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
