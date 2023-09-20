4d5f964d74ff75b2e296b902858219e609f37d4e
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-09-20 06:44:36 +0300

    adding custom scroll view to book details screen body

--- lib/features/home/presetation/views/widgets/book_details_screen_body.dart
+++ lib/features/home/presetation/views/widgets/book_details_screen_body.dart
@@ -14,65 +14,72 @@ class BookDetailsScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              const CustomBookDetailsAppBar(),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: size.width * 0.2),
-                child: const CustomBookImageItem(),
-              ),
-              const SizedBox(
-                height: 43,
-              ),
-              const Text(
-                'The Jungle Book',
-                style: Styles.titleNormal30,
-              ),
-              const SizedBox(
-                height: 6,
-              ),
-              Opacity(
-                opacity: 0.7,
-                child: Text(
-                  'Rudyard Kipling',
-                  style: Styles.titleMedium18.copyWith(
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w500,
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                children: [
+                  const CustomBookDetailsAppBar(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: size.width * 0.2),
+                    child: const CustomBookImageItem(),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 18,
-              ),
-              const BookRating(
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-              const SizedBox(height: 37),
-              const BoxAction(),
-              const SizedBox(height: 50),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'You can also like',
-                  style: Styles.titleNormal14.copyWith(
-                    fontWeight: FontWeight.w600,
+                  const SizedBox(
+                    height: 43,
                   ),
-                ),
+                  const Text(
+                    'The Jungle Book',
+                    style: Styles.titleNormal30,
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      'Rudyard Kipling',
+                      style: Styles.titleMedium18.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  const BookRating(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(height: 37),
+                  const BoxAction(),
+                  const Expanded(
+                    child: SizedBox(height: 50),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'You can also like',
+                      style: Styles.titleNormal14.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const BottomBookDetailsScreenListView(),
+                  const SizedBox(height: 40),
+                ],
               ),
-              const SizedBox(height: 16,),
-              const BottomBookDetailsScreenListView(),
-              const SizedBox(height: 40),
-            ],
+            ),
           ),
-        ),
-      ),
+        )
+      ],
     );
   }
 }
-
-
