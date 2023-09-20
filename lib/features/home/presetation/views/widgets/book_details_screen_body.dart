a86551c9e8c8e43b37969cadb1d04c6100dbba3a
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-09-20 06:29:24 +0300

    create bottom book details screen list view

--- lib/features/home/presetation/views/widgets/book_details_screen_body.dart
+++ lib/features/home/presetation/views/widgets/book_details_screen_body.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/features/home/presetation/views/widgets/custom_book_i
 import 'package:bookly_app/features/home/presetation/views/widgets/rating_book.dart';
 import 'package:flutter/material.dart';
 
+import 'bottom_book_details_screen_list_view.dart';
 import 'box_action.dart';
 import 'custom_book_details_app_bar.dart';
 
@@ -16,44 +17,62 @@ class BookDetailsScreenBody extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          children: [
-            const CustomBookDetailsAppBar(),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: size.width * 0.2),
-              child: const CustomBookImageItem(),
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            const Text(
-              'The Jungle Book',
-              style: Styles.titleNormal30,
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            Opacity(
-              opacity: 0.7,
-              child: Text(
-                'Rudyard Kipling',
-                style: Styles.titleMedium18.copyWith(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              const CustomBookDetailsAppBar(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: size.width * 0.2),
+                child: const CustomBookImageItem(),
+              ),
+              const SizedBox(
+                height: 43,
+              ),
+              const Text(
+                'The Jungle Book',
+                style: Styles.titleNormal30,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Opacity(
+                opacity: 0.7,
+                child: Text(
+                  'Rudyard Kipling',
+                  style: Styles.titleMedium18.copyWith(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            const BookRating(
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            const SizedBox(height: 37),
-            const BoxAction()
-          ],
+              const SizedBox(
+                height: 18,
+              ),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(height: 37),
+              const BoxAction(),
+              const SizedBox(height: 50),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'You can also like',
+                  style: Styles.titleNormal14.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16,),
+              const BottomBookDetailsScreenListView(),
+              const SizedBox(height: 40),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
---
+++ lib/features/home/presetation/views/widgets/bottom_book_details_screen_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'custom_book_image_item.dart';
+class BottomBookDetailsScreenListView extends StatelessWidget {
+  const BottomBookDetailsScreenListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return  SizedBox(
+      height: size.height * 0.15,
+      child: ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: CustomBookImageItem(),
+            );
+          }),
+    );
+  }
+}
